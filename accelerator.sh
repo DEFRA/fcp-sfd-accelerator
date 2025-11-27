99fb373be14ad28aec2f6986eb4b42cc47160ff4
Rana Salem <127218837+rtasalem@users.noreply.github.com>
2025-11-27 11:48:10 +0000

    Update repo template (#7)

--- accelerator.sh
+++ accelerator.sh
@@ -12,21 +12,26 @@ TEMPLATE_REPO="$1"
 TARGET_REPO="$2"
 TARGET_BRANCH="${3:-template-setup}"
 
-TEMP_DIR=$(mktemp -d)
-TEMPLATE_DIR=$(mktemp -d)
+TEMP_TARGET_DIR=$(mktemp -d)
+TEMP_TEMPLATE_DIR=$(mktemp -d)
 
 echo "Cloning target repo..."
-git clone "$TARGET_REPO" "$TEMP_DIR"
-cd "$TEMP_DIR"
+git clone "$TARGET_REPO" "$TEMP_TARGET_DIR"
+cd "$TEMP_TARGET_DIR"
 
 echo "Creating new branch '$TARGET_BRANCH'..."
 git checkout -b "$TARGET_BRANCH"
 
+echo "Deleting all files except .git..."
+shopt -s extglob
+rm -rf !(.git)
+shopt -u extglob
+
 echo "Cloning template repo (accelerator) without history..."
-git clone --depth=1 "$TEMPLATE_REPO" "$TEMPLATE_DIR"
+git clone --depth=1 "$TEMPLATE_REPO" "$TEMP_TEMPLATE_DIR"
 
 echo "Copying files from template repo to target branch..."
-rsync -av --exclude='.git' "$TEMPLATE_DIR"/ ./
+rsync -av --exclude='.git' "$TEMP_TEMPLATE_DIR"/ ./
 
 echo "Adding and committing files..."
 git add .
@@ -38,4 +43,4 @@ git push -u origin "$TARGET_BRANCH"
 echo "Accelerator template has been applied to branch '$TARGET_BRANCH' on $TARGET_REPO"
 
 cd ..
-rm -rf "$TEMP_DIR" "$TEMPLATE_DIR"
+rm -rf "$TEMP_TARGET_DIR" "$TEMP_TEMPLATE_DIR"
